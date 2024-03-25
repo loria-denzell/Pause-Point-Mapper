460ef8cb3795cf4168d45ac290e6c9d69d29c57f
Llezned <loriadenzell@gmail.com>
2024-03-25 14:45:01 +0800

    Exclude __pycache__ folders from version control

--- process/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .activity_parsers.strava_parser import exchange_token
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('exchange_token/', exchange_token, name='exchange_token'),
-]
